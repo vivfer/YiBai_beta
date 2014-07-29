198f2f9238aca67ccc9211fa1e6cb173766cbc7a
yaoliyang <zy@zydeiMac.local>
2014-07-29 20:05:19 +0800

    Initial Commit

---
+++ YiBaiTests/YiBaiTests.m
@@ -0,0 +1,34 @@
+//
+//  YiBaiTests.m
+//  YiBaiTests
+//
+//  Created by yaoliyang on 14-7-29.
+//  Copyright (c) 2014年 yaoliyang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface YiBaiTests : XCTestCase
+
+@end
+
+@implementation YiBaiTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
